e1bc0ea0cea835e53f12204bddb7a585076059dd
Richard Sarohia <RSarohia@hotmail.com>
2020-02-22 15:15:54 -0500

    Third Update

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
-const bodyParser = require('bodyParser');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+var listItems = ["Buy Food","Eat Food","Get More Food"];
 
 app.set('view engine','ejs');
 
 app.get("/", function(req,res){
+
+    var today = new Date();
 
     var options = {
         weekday: "long",
@@ -14,10 +21,17 @@ app.get("/", function(req,res){
     }
 
     var day = today.toLocaleDateString("en-US",options);
+
+    res.render("list", {
+        items: listItems,
+        kindOfDay: day
+    });
+
 });
 
-res.render("list", {
-    kindOfDay: day
+app.post("/", function(req,res){
+    listItems.push(req.body.newItem);
+    res.redirect("/");
 });
 
 app.listen(3000,function(){
